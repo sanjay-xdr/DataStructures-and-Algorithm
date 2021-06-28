d4ed95bef8078f453a4dd9a0fc26a098cadd2e32
sanjay <sanjayjaiswal586@gmail.com>
2021-06-28 17:15:46 +0530

    nCr(also knowen as selection formula

---
+++ Recursion/nCr.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int C(int n,int r){
+	if(r==0||n==r){
+		return 1;
+	}
+	else{
+		return C(n-1,r-1)+C(n-1,r);
+	}
+}
+
+int main(){
+	cout<<C(5,3);
+}
