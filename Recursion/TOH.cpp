ea445e516fc7fa3089d3cbc92cb6cad76264458a
sanjay <sanjayjaiswal586@gmail.com>
2021-06-28 17:40:31 +0530

    Tower of hanoi

---
+++ Recursion/TOH.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int TOH(int n,int A,int B,int C){
+	if(n>0){
+		
+		//move disc from A to B using C
+		TOH(n-1,A,C,B);
+		cout<<"("<<A<<" "<<C <<")";
+		//move disc from B to A using C
+		TOH(n-1,B,A,C);
+	}
+}
+
+int main(){
+	TOH(4,1,2,3);
+	
+}
