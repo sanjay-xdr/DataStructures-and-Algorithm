7a01962e8077d0227dfe28d1db469bb0994f83c7
Sanjay Jaiswal <sanjay.jaiswal@cginfinity.com>
2023-12-22 16:05:29 +0530

    adding levelOrdertraversal

--- Binary Tree/Creation.cpp
+++ Binary Tree/Creation.cpp
@@ -1,7 +1,87 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+class node
+{
+
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+node *buildTree(node *root)
+{
+
+    cout << "Enter the Data " << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+
+    if (data == -1)
+        return NULL;
+
+    cout << "Enter the data for insertion on the left side " << root->data << endl;
+
+    root->left = buildTree(root->left);
+
+    cout << "Enter the data for insertion on the right side " << root->data << endl;
+
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+void LevelOrderTraversal(node *root)
+{
+
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        node *temp = q.front();
+      
+        q.pop();
+
+
+        if(temp==NULL){
+            cout<<endl;
+
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+
+  cout<<temp->data<<" ";
+              if (temp->left)
+        {
+            q.push(temp->left);
+        }
+        if (temp->right)
+        {
+            q.push(temp->right);
+        }
+        }
+
+      
+    }
+}
+
 int main()
 {
 
-    cout << "Hello WOrld";
+    node *root = NULL;
+    root = buildTree(root);
+cout<<"Printing the level Order traversal"<<endl;
+    LevelOrderTraversal(root);
 }
